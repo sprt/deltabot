ae8620690b7411bca856cdab9f5ed07b002164cd
sprt <hellosprt@gmail.com>
2015-08-31 21:15:41 +0200

    Underscore functions == meh

--- tests/deltabot/test_templates.py
+++ tests/deltabot/test_templates.py
@@ -10,7 +10,7 @@ from application.deltabot.utils import render_template
 config.SUBREDDIT = 'testsub'
 
 
-def _get_template_double(path):
+def get_template_double(path):
     data_dir = os.path.join(os.path.dirname(__file__), '../data/templates')
     return open(os.path.join(data_dir, path)).read()
 
@@ -24,7 +24,7 @@ def test_wiki_user_layout():
     rendered_template = render_template('wiki/user_history.md',
                                         username='john',
                                         deltas=[delta] * 2)
-    assert rendered_template == _get_template_double('user_wiki.md')
+    assert rendered_template == get_template_double('user_wiki.md')
 
 
 def test_wiki_tracker_layout():
@@ -33,7 +33,7 @@ def test_wiki_tracker_layout():
                  awarded_to='john',
                  awarder_comment_url='http://example.com/')
     rendered_template = render_template('wiki/tracker.md', deltas=[delta] * 2)
-    assert rendered_template == _get_template_double('wiki_tracker.md')
+    assert rendered_template == get_template_double('wiki_tracker.md')
 
 
 class TemplateTest(unittest.TestCase):
@@ -48,13 +48,13 @@ class TestDeltaAdderCommentTemplate(TemplateTest):
     
     def test_no_error(self):
         rendered = self.render_template(error=None, awardee_username='John')
-        expected = _get_template_double('comments/delta_adder_no_error.md')
+        expected = get_template_double('comments/delta_adder_no_error.md')
         assert rendered == expected
     
     def test_error(self):
         rendered = self.render_template(error='already_awarded',
                                         awardee_username='John')
-        expected = _get_template_double('comments/delta_adder_error.md')
+        expected = get_template_double('comments/delta_adder_error.md')
         assert rendered == expected
 
 
@@ -64,11 +64,11 @@ class TestDeltaRemoverCommentTemplate(TemplateTest):
     def test_remind(self):
         rendered = self.render_template(error=None, awardee_username='John',
                                         removal_reason='remind')
-        expected = _get_template_double('comments/delta_remover_remind.md')
+        expected = get_template_double('comments/delta_remover_remind.md')
         assert rendered == expected
     
     def test_not_remind(self):
         rendered = self.render_template(error=None, awardee_username='John',
                                         removal_reason='abuse')
-        expected = _get_template_double('comments/delta_remover_not_remind.md')
+        expected = get_template_double('comments/delta_remover_not_remind.md')
         assert rendered == expected
